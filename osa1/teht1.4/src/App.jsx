198e67783ff213456e134c3024b9acbc70934aaf
JoonasTMS86 <joonaslindberg@hotmail.com>
2024-04-18 10:03:12 +0300

    Add files via upload

---
+++ osa1/teht1.4/src/App.jsx
@@ -0,0 +1,62 @@
+
+const App = () => {
+  
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  const Header = (props) => {
+    return (
+        <h1>
+          {props.course}
+        </h1>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <Part part={parts[0].name} exercises={parts[0].exercises} />
+        <Part part={parts[1].name} exercises={parts[1].exercises} />
+        <Part part={parts[2].name} exercises={parts[2].exercises} />
+      </div>
+    )
+  }
+  const Total = (props) => {
+    return (
+        <p>
+          Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        </p>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+    </div>
+  )
+}
+
+export default App
