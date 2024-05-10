b4edf6dd3d7d9d73eaa30fa9eec335e8ff60e8fc
JoonasTMS86 <joonaslindberg@hotmail.com>
2024-05-10 13:21:01 +0300

    Add files via upload

---
+++ osa2/teht2.8/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+
+const Person = (props) => {
+  
+  return (
+    <div>
+      {props.name} {props.number} <br/>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      number: '040-1231244'
+    }
+  ]) 
+
+  const [newName, setNewName] = useState(
+    ''
+  )
+
+  const [newNumber, setNewNumber] = useState(
+    ''
+  )
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const names = persons.map(person => person.name)
+    if(names.includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input 
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+          <Person key={person.name} name={person.name} number={person.number} />
+        )}
+    </div>
+  )
+
+}
+
+export default App
